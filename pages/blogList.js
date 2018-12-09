392dc39f5df0dccbcf1086036c5efd7bce613c0c
dingyi1993 <781745492@qq.com>
2018-12-10 01:27:26 +0800

    add blog a-year-from-2017.

--- pages/blogList.js
+++ pages/blogList.js
@@ -1,4 +1,5 @@
 export default [
+  { id: 'a-year-from-2017', datetime: '2018-12-10',title: '时隔一年整的提交', excerpt: '上一次这个博客更新，刚好是在1年前，2017-12-10，实在是惭愧，洗个澡的功夫，决定还是在睡前先写下这段文字，在博客终于迁移到 nuxt 的今天。' },
   { id: 'chrome-devtools-command-line-reference', datetime: '2017-07-14',title: 'chrome devtools中内置的方便调试DOM的函数和变量', banner: '/images/posts/chrome-devtools-command-line-reference/console-panel.png', excerpt: 'chrome的devtools内置了很多方便调试DOM的函数和变量，比如$、$$、$x、debug等，这应该算是一篇伪翻译，因为是从谷歌开发者网站搬过来的（摊手）。' },
   { id: 'fiddler-cross-origin', datetime: '2017-07-08',title: 'fiddler配置可视化的跨域选择', excerpt: '在用fiddler的autoResponder代理接口时，如果接口恰好和你的资源不是同一主机名下，就会产生跨域的问题，此博客介绍如何方便、可视化地添加跨域。' },
   { id: 'browser-sync-ui', datetime: '2017-01-14',title: 'browser-sync中的一些不为人之的秘密', banner: '/images/posts/browser-sync-ui/6.jpg', excerpt: 'web前端开发中的脚手架，大家基本都会用到browser-sync，但是我发现周围好多人仅仅只知道它能实现自动刷新的功能，然而它还有很多更加强大的功能。' },
